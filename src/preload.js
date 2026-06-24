c262e6c58fbd24ee4e29eab1b444ea89217cbcfc
Rakshak Mathur <109144059+RakshakIT@users.noreply.github.com>
2026-06-24 15:54:25 +0100

    Update preload.js

--- src/preload.js
+++ src/preload.js
@@ -1,9 +1,13 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('cookieScanner', {
-  startScan: (options) => ipcRenderer.invoke('scan:start', options),
-  saveFile: (payload) => ipcRenderer.invoke('file:save', payload),
-  onProgress: (callback) => {
+  startScan: options => ipcRenderer.invoke('scan:start', options),
+
+  saveFile: payload => ipcRenderer.invoke('file:save', payload),
+
+  savePdf: payload => ipcRenderer.invoke('pdf:save', payload),
+
+  onProgress: callback => {
     ipcRenderer.removeAllListeners('scan:progress');
     ipcRenderer.on('scan:progress', (_event, data) => callback(data));
   }
